e8385ddaa4bbcd5e2e958dc7edc417d2618097e7
Matt Svihel <matt.svihel@gmail.com>
2013-12-18 21:29:33 -0600

    Linaro -O3 optimations

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2871,13 +2871,14 @@ static inline int started_after(void *p1, void *p2)
 int cgroup_scan_tasks(struct cgroup_scanner *scan)
 {
 	int retval, i;
-	struct cgroup_iter it;
-	struct task_struct *p, *dropped;
+	struct cgroup_iter it = {0};
+	struct task_struct *p = 0, *dropped;
 	/* Never dereference latest_task, since it's not refcounted */
 	struct task_struct *latest_task = NULL;
 	struct ptr_heap tmp_heap;
 	struct ptr_heap *heap;
 	struct timespec latest_time = { 0, 0 };
+        it.task = NULL;
 
 	if (scan->heap) {
 		/* The caller supplied our heap and pre-allocated its memory */
@@ -3212,8 +3213,9 @@ int cgroupstats_build(struct cgroupstats *stats, struct dentry *dentry)
 {
 	int ret = -EINVAL;
 	struct cgroup *cgrp;
-	struct cgroup_iter it;
+	struct cgroup_iter it = {0};
 	struct task_struct *tsk;
+        it.task = NULL;
 
 	/*
 	 * Validate dentry by checking the superblock operations,
